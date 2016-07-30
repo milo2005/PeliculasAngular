c8a1a283a7d7146f9a3eff4e8425908bed321099
System Administrator <root@MacBook-Pro-de-Portatil-3.local>
2016-07-29 21:37:38 -0500

    form ngModel added

--- app/login/login.component.ts
+++ app/login/login.component.ts
@@ -1,14 +1,19 @@
 import {Component} from '@angular/core';
 import {Router} from '@angular/router';
+import {User} from './shared/user';
+import {JsonPipe} from '@angular/common';
 
 @Component({
-    templateUrl:"./app/login/login.component.html"
+    templateUrl:"./app/login/login.component.html",
+    pipes:[JsonPipe]
 })
 export class LoginComponent{
 
     reg:boolean;
+    user:User;
 
     constructor(private router:Router){
+        this.user = new User();
         this.reg = false;
     }
 
